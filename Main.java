d69c2ad9ee5eaeafe3af3a0186b963fb12cec867
Mahdi HajEbrahimi <MahdiHajEbrahimiNasab@gmail.com>
2026-06-02 22:06:24 +0330

    nothing actually

--- Main.java
+++ Main.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 import controller.GameController;
 
 public class Main {
+    Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         GameController.start();
     }
 }
