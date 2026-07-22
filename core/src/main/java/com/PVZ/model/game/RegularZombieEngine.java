1cc8dd17a1e11eca8c645b9a0a27a5c7ca8b23df
Fatemeh Mostafavi <fatemehmostafavi0685@gmail.com>
2026-07-22 14:56:12 +0330

    fix the bug about seen zombies on CollectionMenu

--- core/src/main/java/com/PVZ/model/game/RegularZombieEngine.java
+++ core/src/main/java/com/PVZ/model/game/RegularZombieEngine.java
@@ -5,6 +5,8 @@ import com.PVZ.model.entity.Tile;
 import com.PVZ.model.entity.plants.behavior.impl.Projectile;
 import com.PVZ.model.entity.zombies.base.Zombie;
 import com.PVZ.model.enums.ZombieType;
+import com.PVZ.model.status.AppStatus;
+import com.PVZ.model.user.UserRegistry;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import java.util.ArrayList;
@@ -111,6 +113,11 @@ public class RegularZombieEngine implements ZombieEngine{
         zombie.initPosition(x, y, row);
         zombie.onSpawn();
         zombies.add(zombie);
+        if (AppStatus.currentUser != null && AppStatus.currentUser.collectionState != null) {
+            ZombieType type = ZombieType.fromAlias(alias);
+            AppStatus.currentUser.collectionState.seeZombie(type);
+            UserRegistry.touch(AppStatus.currentUser.profile.getUsername());
+        }
         return zombie;
     }
 
