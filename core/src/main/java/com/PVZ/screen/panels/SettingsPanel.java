cfedb2778ae9a2669a0473750ac6bb4032ebf279
Mahdi HajEbrahimi <MahdiHajEbrahimiNasab@gmail.com>
2026-08-03 17:52:18 +0330

    so many new things such as panelManager and panels and etc, the background is still a copy of hollow knight because my Veepn stopped working, Make sure all sound effects and SFXs go through soundMangager, some panles are still available for instance especially Settings

---
+++ core/src/main/java/com/PVZ/screen/panels/SettingsPanel.java
@@ -0,0 +1,210 @@
+//package com.PVZ.screen.panels;
+//
+//import com.badlogic.gdx.Gdx;
+//import com.badlogic.gdx.graphics.Texture;
+//import com.badlogic.gdx.utils.Align;
+//import com.PVZ.HollowKnightGame;
+//import com.PVZ.model.enums.MenuType;
+//import com.PVZ.model.status.AppStatus;
+//import com.PVZ.screen.GameScreen;
+//import com.PVZ.screen.manager.FontManager;
+//import com.PVZ.screen.manager.PanelManager;
+//import com.PVZ.screen.ui.MenuButton;
+//import com.PVZ.screen.ui.MenuSlider;
+//import com.PVZ.screen.ui.SliderBinding;
+//import com.PVZ.screen.ui.ToggleBinding;
+//import com.PVZ.view.CommandSender;
+//import com.PVZ.view.InputDTOs.settingMenuInputDTOs.QualityChangeInputDTO;
+//import com.PVZ.view.InputDTOs.settingMenuInputDTOs.ResetVolumeInputDTO;
+//
+//import java.util.jar.Manifest;
+//
+//public class SettingsPanel extends BasePanel {
+//
+//    public Texture soundOnTex;
+//    public Texture soundOffTex;
+//
+//    private final MenuSlider musicSlider;
+//    private final MenuSlider sfxSlider;
+//    private final MenuSlider brightnessSlider;
+//
+//    private final MenuButton qualityButton;
+//    private boolean toPauseMenu = false;
+//
+//    public SettingsPanel(boolean toPauseMenu) {
+//        this();
+//        this.toPauseMenu = toPauseMenu;
+//    }
+//
+//    public SettingsPanel() {
+//        this.setFillParent(true);
+//        this.align(Align.center);
+//
+//        // عنوان
+//        Texture titleTexture = new Texture(Gdx.files.internal("SettingsMenu/SettingTittle.png"));
+//        addArt(titleTexture, 670f, 870f, 1220f, 520f);
+//        this.padTop(400f);
+//
+//        float spacing = 40f;
+//
+//        soundOnTex = new Texture(Gdx.files.internal("global/sound/sound_on.png"));
+//        soundOffTex = new Texture(Gdx.files.internal("global/sound/sound_off.png"));
+//
+//        // ==========================================
+//        // ۱. اسلایدر حجم موسیقی (Music Volume)
+//        // ==========================================
+//        musicSlider = new MenuSlider("Music Volume",
+//            FontManager.getInstance().getEnglishMenuFont(),
+//            null, null, null,
+//            new Texture("global/button_marker.png"),
+//            true, // 🌟 hasIcon = true
+//            soundOnTex,
+//            soundOffTex,
+//            new SliderBinding() {
+//                @Override
+//                public int get() {
+//                    return AppStatus.getMusicVolume();
+//                }
+//
+//                @Override
+//                public void set(int value) {
+//                    AppStatus.setMusicVolume(value);
+//                }
+//            },
+//            new ToggleBinding() {
+//                @Override
+//                public boolean get() {
+//                    return AppStatus.getMuteMusic();
+//                }
+//
+//                @Override
+//                public void set(boolean value) {
+//                    AppStatus.setMutedMusic(value);
+//                }
+//            });
+//        this.add(musicSlider).padBottom(spacing - 20f).row();
+//
+//        // ==========================================
+//        // ۲. اسلایدر حجم افکت‌ها (SFX Volume)
+//        // ==========================================
+//        sfxSlider = new MenuSlider("SFX Volume",
+//            FontManager.getInstance().getEnglishMenuFont(),
+//            null, null, null,
+//            new Texture("global/button_marker.png"),
+//            true, // 🌟 hasIcon = true
+//            soundOnTex,
+//            soundOffTex,
+//            new SliderBinding() {
+//                @Override
+//                public int get() {
+//                    return AppStatus.getSFXVolume();
+//                }
+//
+//                @Override
+//                public void set(int value) {
+//                    AppStatus.setSFXVolume(value);
+//                }
+//            },
+//            new ToggleBinding() {
+//                @Override
+//                public boolean get() {
+//                    return AppStatus.getMuteSFX();
+//                }
+//
+//                @Override
+//                public void set(boolean value) {
+//                    AppStatus.setMutedSFX(value);
+//                }
+//            });
+//
+//        this.add(sfxSlider).padBottom(spacing - 20f).row();
+//
+//        // ==========================================
+//        // ۳. اسلایدر روشنایی (Brightness)
+//        // ==========================================
+//
+//        brightnessSlider = new MenuSlider("Brightness",
+//            FontManager.getInstance().getEnglishMenuFont(),
+//            null, null, null,
+//            new Texture("global/button_marker.png"),
+//            false, // 🌟 hasIcon = false (دکمه را مخفی کن)
+//            null,
+//            null,
+//            new SliderBinding() {
+//                @Override
+//                public int get() {
+//                    return AppStatus.getBrightness();
+//                }
+//
+//                @Override
+//                public void set(int value) {
+//                    AppStatus.setBrightness(value);
+//                }
+//            },
+//            null // 🌟 نیازی به ToggleBinding نیست
+//        );
+//
+//        this.add(brightnessSlider).padBottom(spacing - 20f).row();
+//
+//        // ==========================================
+//        // ۴. دکمه‌های تنظیمات
+//        // ==========================================
+//        qualityButton = new MenuButton(
+//            "Quality: " + ((HollowKnightGame) Gdx.app.getApplicationListener()).getCurrentQuality().name(),
+//            FontManager.getInstance().getEnglishMenuFont(),
+//            this::onQualityChange);
+//
+//        this.add(qualityButton).padBottom(spacing).row();
+//
+//        this.add(new MenuButton("RESET SOUNDS",
+//            FontManager.getInstance().getEnglishMenuFont(),
+//            this::onResetSounds)).padBottom(spacing).row();
+//
+//        this.add(new MenuButton("KEYBOARD",
+//            FontManager.getInstance().getEnglishMenuFont(),
+//            this::onKeyboard)).padBottom(spacing).row();
+//
+//
+//        this.add(new MenuButton("LANGUAGE",
+//            FontManager.getInstance().getEnglishMenuFont(),
+//            this::onLanguage)).padBottom(spacing).row();
+//
+//        // بازگشت
+//        this.add(new MenuButton("BACK",
+//            FontManager.getInstance().getEnglishMenuFont(),
+//            this::onBack)).row();
+//    }
+//
+//    private void onQualityChange() {
+//        CommandSender.send(new QualityChangeInputDTO(qualityButton));
+//    }
+//
+//    private void onResetSounds() {
+//        CommandSender.send(new ResetVolumeInputDTO(musicSlider, sfxSlider, brightnessSlider));
+//    }
+//
+//    private void onKeyboard() {
+//
+//        AppStatus.setCurreMenu(MenuType.KEYBOARD);
+//        PanelManager.getInstance().performPanelTransition(new KeyboardPanel(toPauseMenu));
+//    }
+//
+//    private void onLanguage() {
+//        System.out.println("[Settings] Language change.");
+//    }
+//
+//    private void onBack() {
+//        if (!toPauseMenu) AppStatus.ChangeMenuAndPanel(MenuType.MAIN_MENU);
+//        else PanelManager.getInstance().performPanelTransition(new PausePanel((GameScreen) AppStatus.getScreen()));
+//    }
+//
+//    @Override
+//    public void dispose() {
+//        // جلوگیری از مموری لیک تکستچرهای آیکون صدا
+//        if (soundOnTex != null)
+//            soundOnTex.dispose();
+//        if (soundOffTex != null)
+//            soundOffTex.dispose();
+//        super.dispose();
+//    }
+//}
---
+++ core/src/main/java/com/PVZ/screen/ui/MenuButton.java
@@ -0,0 +1,167 @@
+package com.PVZ.screen.ui;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.PVZ.screen.manager.CursorManager;
+import com.PVZ.screen.manager.SoundManager;
+
+public class MenuButton extends Actor {
+    private String text;
+    private BitmapFont font;
+    private Texture markerTexture;
+    private Runnable clickAction;
+    private GlyphLayout textLayout;
+
+    private boolean isHovered = false;
+    private float currentAlpha = 0.5f;
+    private final float TARGET_HOVER_ALPHA = 1.0f;
+    private final float FADE_SPEED = 5f;
+    private final float PADDING = 25f;
+
+    private static Texture defaultMarker;
+    private static Sound hoverSound;
+    private static Sound clickSound;
+
+    public MenuButton(String text, BitmapFont font, Runnable clickAction) {
+        this(text, font, getDefaultMarker(), clickAction);
+    }
+
+    public MenuButton(String text, BitmapFont font, Texture markerTexture, Runnable clickAction) {
+        this.text = text;
+        this.font = font;
+        this.markerTexture = markerTexture;
+        this.clickAction = clickAction;
+
+        this.textLayout = new GlyphLayout(font, text);
+        float dynamicWidth = textLayout.width + 150f;
+        setSize(dynamicWidth, 50);
+
+        addListener(new ClickListener() {
+            @Override
+            public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor) {
+                if (pointer == -1 && !isHovered) {
+                    // getHoverSound().play();
+                    // 🌟 افکت هاور با رعایت وضعیت سیستم میوت از طریق متد playSound پخش می‌شود
+                    SoundManager.getInstance().playSound(getHoverSound());
+                }
+                isHovered = true;
+
+                CursorManager.getInstance().setPointerMode(true);
+            }
+
+            @Override
+            public void exit(InputEvent event, float x, float y, int pointer, Actor toActor) {
+                isHovered = false;
+
+                CursorManager.getInstance().setPointerMode(false);
+            }
+
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                // getClickSound().play();
+                // 🌟 افکت کلیک با رعایت وضعیت سیستم میوت از طریق متد playSound پخش می‌شود
+                SoundManager.getInstance().playSound(getClickSound());
+
+                if (MenuButton.this.clickAction != null) {
+                    MenuButton.this.clickAction.run();
+                }
+            }
+        });
+    }
+
+    private static Texture getDefaultMarker() {
+        if (defaultMarker == null) {
+            defaultMarker = new Texture(Gdx.files.internal("global/button_marker.png"));
+        }
+        return defaultMarker;
+    }
+
+    private static Sound getHoverSound() {
+        if (hoverSound == null) {
+            hoverSound = Gdx.audio.newSound(Gdx.files.internal("global/BottomSelection.mp3"));
+        }
+        return hoverSound;
+    }
+
+    private static Sound getClickSound() {
+        if (clickSound == null) {
+            clickSound = Gdx.audio.newSound(Gdx.files.internal("global/BottomClicked.mp3"));
+        }
+        return clickSound;
+    }
+
+    public static void disposeDefault() {
+        if (defaultMarker != null) {
+            defaultMarker.dispose();
+            defaultMarker = null;
+        }
+        if (hoverSound != null) {
+            hoverSound.dispose();
+            hoverSound = null;
+        }
+        if (clickSound != null) {
+            clickSound.dispose();
+            clickSound = null;
+        }
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+        float targetAlpha = isHovered ? TARGET_HOVER_ALPHA : 0.5f;
+        currentAlpha += (targetAlpha - currentAlpha) * FADE_SPEED * delta;
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        font.setColor(1f, 1f, 1f, currentAlpha);
+
+        float textX = getX() + (getWidth() - textLayout.width) / 2f;
+        float textY = getY() + (getHeight() + textLayout.height) / 2f;
+
+        font.draw(batch, text, textX, textY);
+
+        if (isHovered && markerTexture != null) {
+            batch.setColor(1f, 1f, 1f, currentAlpha);
+
+            float markerWidth = 70f;
+            float markerHeight = 44f;
+            float dynamicPadding = PADDING + 10f;
+            float markerY = getY() + (getHeight() - markerHeight) / 2f;
+
+            batch.draw(markerTexture,
+                    textX - dynamicPadding - markerWidth,
+                    markerY,
+                    markerWidth,
+                    markerHeight);
+
+            batch.draw(markerTexture,
+                    textX + textLayout.width + dynamicPadding,
+                    markerY,
+                    markerWidth,
+                    markerHeight,
+                    0, 0,
+                    markerTexture.getWidth(), markerTexture.getHeight(),
+                    true, false);
+
+            batch.setColor(Color.WHITE);
+        }
+    }
+
+    //Dynamic botton
+    public void setText(String newText) {
+        this.text = newText;
+        this.textLayout.setText(font, text);
+        float dynamicWidth = textLayout.width + 150f;
+        setSize(dynamicWidth, getHeight());
+    }
+
+}
