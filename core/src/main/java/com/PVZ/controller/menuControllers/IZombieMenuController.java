f551a3f8f159f60bec97b7ac3124bb3a4458cce5
Mahdi HajEbrahimi <MahdiHajEbrahimiNasab@gmail.com>
2026-08-12 16:02:36 +0330

    map background fixed

--- core/src/main/java/com/PVZ/controller/menuControllers/IZombieMenuController.java
+++ core/src/main/java/com/PVZ/controller/menuControllers/IZombieMenuController.java
@@ -47,7 +47,7 @@ public class IZombieMenuController {
         currentGame = new IZombieGame(level);
         currentEngine.setGame(currentGame);
 
-        Map map = new Map(550, 1240, 1600, 1170, currentGame.getRows(), currentGame.getCols());
+        Map map = new Map(480, 1235, 1655, 1170, currentGame.getRows(), currentGame.getCols());
         currentEngine.setMap(map);
         currentEngine.initializeBoard();
 
